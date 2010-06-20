caa87e5e5fca11590396a8baacbdcdc2cd1433ca
Christoph Burgmer <cburgmer@ira.uka.de>
2010-06-20 21:26:23 +0000

    [characterdb] Add initial stroke order rules set
    git-svn-id: https://cjklib.googlecode.com/svn/trunk@415 fa4245f0-ad46-11dd-9d36-519045110ae9

--- characterdb/CharacterDB/languages/CDB_Messages.php
+++ characterdb/CharacterDB/languages/CDB_Messages.php
@@ -12,4 +12,5 @@ $messages['en'] = array(
 	// user messages
 	'cdb-desc'                         => 'Parser functions and input types for CharacterDB',
 	'cdb_bad_decomposition'            => 'invalid decomposition',
+	'cdb-strokeorder-rules'            => "⿰ 1 2\n⿲ 1 2\n⿱ 1 2\n⿳ 1 2\n⿵ 1 2\n⿸ 1 2\n⿹ 1 2\n⿺ 1 2\n⿶ 2 1\n⿴ S-HZ 2 H | 1 is 囗 | 1 has S-HZ-H\n",
 );
